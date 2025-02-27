f5cee8f8c8283f8e54f6920060ea40f23a68c2bf
Frontend JavaScript <frontendjavascript@iMAC-10.local>
2025-02-27 20:46:45 +0200

    curs7

---
+++ curs7/mywebsite/src/index.js
@@ -0,0 +1,6 @@
+// Creaza un obiect Date
+var currentDate = new Date();
+// Obtine anul curent
+var currentYear = currentDate.getFullYear();
+// Afiseaza anul curent in elementul cu id "currentYear"
+document.getElementById('currentYear').textContent = currentYear;
